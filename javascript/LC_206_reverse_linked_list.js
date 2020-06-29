41e4925339c2d99b8f93a3d41210254f7ab4564a
mbildner <mbildner@icloud.com>
2020-06-29 12:02:58 -0400

    review 206 reverse linked list, update excel doc

--- javascript/LC_206_reverse_linked_list.js
+++ javascript/LC_206_reverse_linked_list.js
@@ -2,78 +2,68 @@
 // EASY
 // Reverse a singly linked list
 // https://leetcode.com/problems/reverse-linked-list/
-// Input:  1 -> 2 -> 3 -> 4 -> 5 -> NULL
-// Output: 5 -> 4 -> 3 -> 2 -> 1 -> NULL
+// Input:  1 -> 2 -> 3 -> 4 -> 5 -> NULL			takes in HEAD NODE
+// Output: 5 -> 4 -> 3 -> 2 -> 1 -> NULL			returns HEAD NODE
 
 // Definition for singly linked list
 function ListNode(val) {
-    this.val = val;
-    this.next = null;
+	this.val = val;
+	this.next = null;
 }
 
-// SOLUTION V1- Iterative- O(n) Time, O(1) Space, where n = list length
-// Non Mutative of input head?
+// SOLUTION V1- MY SOLUTION, iterative, mutates list/in place, more temp variables
+// TIME COMPLEXITY: O(n),   n = list length
+// SPACE CPMLEXITY: O(1)
 // Takes in head list node, returns new head where subsequent nodes are reversed from original
-const reverseList = function(head) {
-    // create var to track previousNode = null
-    let previousNode = null;
+const reverseListV1 = function(head) {
+	// create var to track previousNode = null
+	let previousNode = null;																		
 
-    // create var to track currentNode = head
-    let currentNode = head;
+	// create var to track currentNode = head
+	let currentNode = head;
 
-    // traverse linked list
-    while (currentNode !== null) {
-        // create temp var to capture next node
-        let temp = currentNode.next;                // need this for reassigning currentNode later
+	// traverse linked list
+	while (currentNode !== null) {
+		// create temp var to capture next node
+		let temp = currentNode.next;                // need this for reassigning currentNode later
 
-        // set currentNode point to previousNode
-        currentNode.next = previousNode;
+		// set currentNode point to previousNode
+		currentNode.next = previousNode;
 
-        // set previousNode to currentNode
-        previousNode = currentNode;
+		// set previousNode to currentNode
+		previousNode = currentNode;
 
-        // set currentNode to temp
-        currentNode = temp;
-    }
+		// set currentNode to temp
+		currentNode = temp;
+	}
 
-    // return previousNode
-    return previousNode;
+	// return previousNode
+	return previousNode;
 }
 
 
 
 
 
-// // VERSION2- Iterative O(n) Time, O(1) Space?- MUTATES HEAD?-
-// var reverseList = function (head) {
-//     let prev = null;
+// SOLUTION V1- OPTIMAL LC SOLUTION, iterative, mutates list/in place, less temp variables
+// TIME COMPLEXITY: O(n),   n = list length
+// SPACE CPMLEXITY: O(1)
+// a -> b -> c -> d             d -> c -> b -> a
+// (a) => (d)
+var reverseList = function (head) {
+	let prev = null;																															// 1) create var to track prev node, set to null
 
-//     while (head !== null) {
+	while (head !== null) {																												// 2) loop through all nodes, head will be our current node (need to keep updating it)
 
-//         // Capture next node
-//         let next = head.next;
-//         // 1:   next = 1.next = 2
-//         // 2:   next = 2.next = 3
-//         // 3:   next = 3.next = null
+		let next = head.next;																												// 3) grab next node from head
 
-//         // Re assign Next node to Previous node
-//         head.next = prev;
-//         // 1:   1.next -> null
-//         // 2:   2.next -> 1
-//         // 3:   3.next -> 2
+		head.next = prev;																														// 4) make head point to prev node
 
-//         // Re assign Previous node to Head
-//         prev = head;
-//         // 1:   prev = 1
-//         // 2:   prev = 2
-//         // 3:   prev = 3
+		prev = head;																																// 5) update prev to head
 
-//         // Re assign Head to Next
-//         head = next;
-//         // 1:   head = 2
-//         // 2:   head = 3
-//         // 3:   head = null
-//     }
+		head = next;																																// 6) update head to next
+	}
 
-//     return prev;
-// };
+	return prev;																																	// 7) return prev
+};
+// NOTE* make A "point to" C == make B's next property equal to C (i.e. a.next = c)
