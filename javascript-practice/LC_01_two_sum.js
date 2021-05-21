42a0da5e92a90cd128a7a9e5945e3d52ac690a2b
maxbildner <mbildner@icloud.com>
2021-05-21 14:11:41 -0400

    update LC01 two sum solution

---
+++ javascript-practice/LC_01_two_sum.js
@@ -0,0 +1,41 @@
+// LC 01 Two Sum VERSION 1 (ONLY 1 SOLUTION)
+// EASY
+// Given an array of integers, return indices of the two numbers such that they 
+// add up to a specific target. You may assume that each input would have 
+// exactly one solution, and you may not use the same element twice.
+// https://leetcode.com/problems/two-sum/
+// Ex1.
+// Input:  nums = [2, 7, 11, 15], target = 9,
+// Output:  [0, 1]  Because nums[0] + nums[1] = 2 + 7 = 9
+
+
+// SOLUTION1- Hash Table
+// Time Complexity: O(n), where n = input array length
+// Space Complexity: O(n) 
+// Solution Description: 
+//   0  1  2  3  4
+// [ 2, 7, 3, 0, 6 ], 8   => [ 0, 4 ]
+const twoSum = (array, target) => {
+	
+}
+console.log(twoSum([2, 7, 3, 0, 6], 8));      //=> [0, 4] 
+console.log(twoSum([2, 7, 11, 15], 9));    		//=> [0, 1] 
+console.log(twoSum([3, 2, 4], 6));    		  	//=> [1, 2] 
+
+
+
+
+// ***************************************************************************
+// LC 01 Two Sum VERSION 2 (OUTPUT CAN HAVE MULTIPLE INDICES)
+// SOLUTION1- Hash Table
+// Time Complexity: O(n), where n = input array length
+// Space Complexity: O(n) 
+// Solution Description: 
+//   0  1  2  3  4
+// [ 2, 7, 1, 0, 6 ], 8   => [ [1, 2], [0, 4] ]
+const twoSumV2 = (array, target) => {
+	
+}
+// console.log(twoSumV2([2, 7, 1, 0, 6], 8));        //=> [ [1, 2], [0, 4] ]
+// console.log(twoSumV2([2, 7, 11, 15], 9));         //=> [ [0, 1] ]
+// console.log(twoSumV2([3, 2, 4], 6));    		     	 //=> [ [1, 2] ]
--- javascript-solutions/LC_01_two_sum.js
+++ javascript-solutions/LC_01_two_sum.js
@@ -1,4 +1,4 @@
-// LC 01 Two Sum
+// LC 01 Two Sum VERSION 1 (ONLY 1 SOLUTION)
 // EASY
 // Given an array of integers, return indices of the two numbers such that they 
 // add up to a specific target. You may assume that each input would have 
@@ -13,34 +13,72 @@
 // Time Complexity: O(n), where n = input array length
 // Space Complexity: O(n) 
 // Solution Description: 
-//  0  1  2  3  4
-// [2, 7, 1, 0, 6], 8   => [ [1, 2], [0, 4] ]
+//   0  1  2  3  4
+// [ 2, 7, 3, 0, 6 ], 8   => [ 0, 4 ]
 const twoSum = (array, target) => {
-    let pairs = [];              // to return
-    let complements = {};
+	// create hash table, keys = nums in array, val = corresponding index in array
+	let table = {};
 
-    // loop through each num in array
-    for (let i = 0; i < array.length; i++) {
-        let num = array[i];
-        // i = 0: num = 2
-        // i = 1: num = 7
-        let complement = target - num;
-        // i = 0: complement = 8 - 2 = 6
-        // i = 1: complement = 8 - 7 = 1
+	// loop through array
+	for (let i = 0; i < array.length; i++) {
+		let num = array[i];
+		
+		// calculate complementing number of current num
+		let complement = target - num;
+		
+		// if complementing num is in hashtable, we have a match!
+		if (table[complement] != undefined) {
 
-        // populate complements object with num and current index
-        complements[num] = i;
-        // i = 0: complements = { "2": 0 }
-        // i = 1: complements = { "2": 0, "7":1 }
+			// return pair of indices
+			return [table[complement], i];
+		} 
 
-        // check if complement is a key in complements object, pair found!
-        if (complements[complement] !== undefined) {
-            // i = 0: complements[6] = undefined
-            // i = 1: complements[1] = undefined
-            pairs.push([complements[complement], i]);
-        }
-    }
-
-    return pairs;
+		// populate table with current num and corresponding index
+		table[num] = i;
+	}
 }
-console.log(twoSum([2, 7, 1, 0, 6], 8));        //=> [ [1, 2], [0, 4] ]
+console.log(twoSum([2, 7, 3, 0, 6], 8));     	//=> [0, 4] 
+console.log(twoSum([2, 7, 11, 15], 9));    		//=> [0, 1] 
+console.log(twoSum([3, 2, 4], 6));    		  	//=> [1, 2] 
+
+
+
+
+// ***************************************************************************
+// LC 01 Two Sum VERSION 2 (OUTPUT CAN HAVE MULTIPLE INDICES)
+// SOLUTION1- Hash Table
+// Time Complexity: O(n), where n = input array length
+// Space Complexity: O(n) 
+// Solution Description: 
+//   0  1  2  3  4
+// [ 2, 7, 1, 0, 6 ], 8   => [ [1, 2], [0, 4] ]
+const twoSumV2 = (array, target) => {
+	// create hash table, keys = nums in array, val = corresponding index in array
+	let table = {};
+
+	// create empty array to return 
+	let pairs = [];
+
+	// loop through array
+	for (let i = 0; i < array.length; i++) {
+		let num = array[i];
+		
+		// calculate complementing number of current num
+		let complement = target - num;
+		
+		// if complementing num is in hashtable, we have a match!
+		if (table[complement] != undefined) {
+
+			// add pairs to array
+			pairs.push([table[complement], i]);
+		} 
+
+		// populate table with current num and corresponding index
+		table[num] = i;
+	}
+
+	return pairs;
+}
+// console.log(twoSumV2([2, 7, 1, 0, 6], 8));       //=> [ [1, 2], [0, 4] ]
+// console.log(twoSumV2([2, 7, 11, 15], 9));        //=> [ [0, 1] ]
+// console.log(twoSumV2([3, 2, 4], 6));    		     	//=> [ [1, 2] ]
