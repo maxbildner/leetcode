72f0d25c9344324f47686352e2ca184d241b0d8b
maxbildner <mbildner@icloud.com>
2021-07-20 14:20:12 -0400

    update LC1, LC206 progress

--- javascript-practice/LC_01_two_sum.js
+++ javascript-practice/LC_01_two_sum.js
@@ -34,7 +34,7 @@ console.log(twoSum([3, 2, 4], 6));    		  	//=> [1, 2]
 //   0  1  2  3  4
 // [ 2, 7, 1, 0, 6 ], 8   => [ [1, 2], [0, 4] ]
 const twoSumV2 = (array, target) => {
-	
+
 }
 // console.log(twoSumV2([2, 7, 1, 0, 6], 8));        //=> [ [1, 2], [0, 4] ]
 // console.log(twoSumV2([2, 7, 11, 15], 9));         //=> [ [0, 1] ]
---
+++ javascript-practice/LC_206_reverse_linked_list.js
@@ -0,0 +1,54 @@
+// LC 206 Reverse Linked List
+// EASY
+// Reverse a singly linked list
+// https://leetcode.com/problems/reverse-linked-list/
+// Input:  1 -> 2 -> 3 -> 4 -> 5 -> NULL			takes in HEAD NODE
+// Output: 5 -> 4 -> 3 -> 2 -> 1 -> NULL			returns HEAD NODE
+
+// Definition for singly linked list
+function ListNode(val) {
+	this.val = val;
+	this.next = null;
+}
+
+
+// SOLUTION V1- OPTIMAL LC SOLUTION, iterative, mutates list/in place, less temp variables
+// TIME COMPLEXITY: O(n),   n = list length
+// SPACE CPMLEXITY: O(1)
+// a -> b -> c -> d             d -> c -> b -> a
+// (a) => (d)
+var reverseList = function (head) {
+	
+};
+// NOTE* make A "point to" C == make B's next property equal to C (i.e. a.next = c)
+
+
+// EXAMPLE 1
+let a = new ListNode('a');
+let b = new ListNode('b');
+let c = new ListNode('c');
+a.next = b;
+b.next = c;
+console.log(printList(reverseList(a)));		// 'c -> b -> a'
+
+
+function printList(n) {
+	let str = '';
+	while (n) {
+
+		if (n.next) {
+			str += n.val + ' -> ';
+		} else {
+			str += n.val;
+		}
+
+		n = n.next;
+	}
+
+	return str;
+}
+// console.log(printList(a));		// 'a -> b -> c'
+
+
+
+
